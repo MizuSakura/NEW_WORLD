20a8dc27f902ca7df0dde90519395a6521aa4517
Mizusakura <Phakhaphun.rodrunda@gmail.com>
2025-12-18 15:09:00 +0700

    init(file) MQTT FILE

---
+++ notebooks/ex11_online_comucation/MQTT_PUSHER.PY
@@ -0,0 +1,8 @@
+import paho.mqtt.client as mqtt
+
+# --- Config ---
+BROKER = "100.85.77.73"
+PORT = 1883
+USER, PASS = "yessuskhonpui", "246810"
+TOPIC = "RL/COMMAND_CONTROL/ACTION"
+CLIENT_ID = "python-subscriber-01"
