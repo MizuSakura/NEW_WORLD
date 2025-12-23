a7361e7671fcc47ee0f19aff7e61c136853106ca
Mizusakura <Phakhaphun.rodrunda@gmail.com>
2025-12-23 17:49:47 +0700

    feat: enhance MQTT subscriber with connection and message handling functions

--- notebooks/ex11_online_comucation/MQTT_SUBSCRIBER.py
+++ notebooks/ex11_online_comucation/MQTT_SUBSCRIBER.py
@@ -1,8 +1,29 @@
 import paho.mqtt.client as mqtt
 
-# --- Config ---
 BROKER = "100.85.77.73"
 PORT = 1883
 USER, PASS = "yessuskhonpui", "246810"
 TOPIC = "RL/COMMAND_CONTROL/ACTION"
 CLIENT_ID = "python-subscriber-01"
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT Broker")
+        client.subscribe(TOPIC, qos=1)
+    else:
+        print("Connection failed:", rc)
+
+def on_message(client, userdata, msg):
+    action = msg.payload.decode()
+    print(f"Receive action -> {action}")
+    # ตรงนี้คือจุดที่คุณเอา action ไปใช้จริง
+    # เช่น ส่งไป PLC / Modbus / Plant
+
+client = mqtt.Client(client_id=CLIENT_ID)
+client.username_pw_set(USER, PASS)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(BROKER, PORT, keepalive=60)
+client.loop_forever()
